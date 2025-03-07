f5bc56179293e6e94f4cf2c84387a990be9ca7ae
Priyankesh <priyankeshom@gmail.com>
2025-03-07 20:24:35 +0530

    Enable SSL for PostgreSQL connection

--- server/db.ts
+++ server/db.ts
@@ -15,6 +15,9 @@ export const pool = new Pool({
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
   connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
+  ssl: {
+    rejectUnauthorized: false // Required for Render's PostgreSQL
+  }
 });
 
 // Add error handling for the pool
